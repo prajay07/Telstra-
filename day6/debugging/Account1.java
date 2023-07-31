bf02c1aafb398eae8e658dd84c5471782d87eda9
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:13:01 +0530

    Update Account1.java

--- day6/debugging/Account1.java
+++ day6/debugging/Account1.java
@@ -1,1 +1,58 @@
+package com.telstra.day6_debugging;
 
+
+class Account1
+{
+	int id; 
+	String name;
+	int balance;
+	
+	
+public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+public Account1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+public Account1(int id, String name, int balance) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.balance = balance;
+}
+
+
+
+
+public class Account{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+}
