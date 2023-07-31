b4d0972ba4888796df9563d6d39190b9cb0c5de6
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:13:26 +0530

    Update Demo1.java

--- day6/debugging/Demo1.java
+++ day6/debugging/Demo1.java
@@ -1,1 +1,16 @@
+package com.telstra.day6_debugging;
 
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for(int i=1;i<=5;i++)
+		{
+			sum=sum+i;
+		
+		}
+	  System.out.println("sum:"+sum);
+	}
+
+}
