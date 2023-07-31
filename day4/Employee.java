1588e1464e1c1409ed4284bc3b8aa3fe4dc444f9
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 23:11:01 +0530

    Add files via upload

---
+++ day4/Employee.java
@@ -0,0 +1,28 @@
+package telstra.day4.encapsulation;
+
+public class Employee {
+	
+    private int id;
+    private String name;
+	
+	public int getId()
+	{
+		return id;
+	}
+	public void setId()
+	{
+		this.id=id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName()
+	{
+		this.name=name;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ day4/encapsulationDemo.java
@@ -0,0 +1,22 @@
+package telstra.day4.encapsulation;
+
+public class encapsulationDemo {
+
+	public static void main(String[] args) {
+		
+		
+		Employee employee=new Employee();
+		
+		
+	employee.setId();
+	employee.setName();
+		
+		
+		System.out.println(employee.getId());
+		
+		System.out.println(employee.getName());
+		
+	
+	}
+
+}
