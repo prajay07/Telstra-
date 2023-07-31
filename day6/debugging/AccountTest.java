c33ba778ce780c61bda637834f7271655db49efb
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:14:19 +0530

    Update AccountTest.java

--- day6/debugging/AccountTest.java
+++ day6/debugging/AccountTest.java
@@ -1,1 +1,19 @@
+package com.telstra.day6_debugging;
 
+
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		Account1 account=new Account1();
+		
+		account.getBalance();
+		
+		account.setId(1);
+		account.setName("ramesh");
+		account.setBalance(2000);
+		
+		account.getBalance();
+	}
+
+}
