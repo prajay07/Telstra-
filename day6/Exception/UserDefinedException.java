493037faa41c2df60bbd00332e30bad65cd3a88e
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:50:43 +0530

    Update UserDefinedException.java

--- day6/Exception/UserDefinedException.java
+++ day6/Exception/UserDefinedException.java
@@ -1,1 +1,54 @@
+package com.telstra.day6;
+
+ 
+//For custom exception -
+class InValidAgeException extends RuntimeException
+{
+	String msg;
+
+	public InValidAgeException(String msg) {
+		super();
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+//normal Exception
+class Customer
+{
+	public void CheckEligibility(int age) {
+		if(age>=18)
+			System.out.println("Elgible to vote");
+		else
+			throw new InValidAgeException("age is less,Minimum should be 18");
+	
+}
+}	
+	
+
+//Main function used to call:
+public class UserDefinedException 
+{
+	
+	public  void main(String[] args) 
+	{
+		Customer customer = new Customer();
+		
+		try 
+		{ 
+			customer.CheckEligibility(23); 
+		} 
+	    catch(InValidAgeException e) 
+		{ 
+			System.out.println(e.getMsg()); 
+		}
+	}
+}
+}
 
