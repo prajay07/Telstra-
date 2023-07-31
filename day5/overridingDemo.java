c4cbc6355092838aa8aab129da1a37d7ed57165e
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 23:00:37 +0530

    Update overridingDemo.java

--- day5/overridingDemo.java
+++ day5/overridingDemo.java
@@ -1,1 +1,42 @@
+package telstra.day5.staticDemo;
+
+class RBI
+{
+	void holidays() {
+		System.out.println("2nd and 4th Weekend");
+	}
+	void dailylimit() {
+		System.out.println("1 lakh");
+	}
+
+}
+class SBI extends RBI
+{
+	void dailylimit() {
+		System.out.println("2 lakh");
+	}
+	void sbirule() {
+		System.out.println("All the day working");
+	}
+
+}
+
+
+
+
+public class overridingDemo {
+
+	public static void main(String[] args) {
+		SBI sbi=new SBI();
+		sbi.dailylimit();
+		sbi.holidays();
+		sbi.sbirule();
+		
+		
+		RBI sbi2=new RBI();
+		sbi2.dailylimit();
+		sbi2.holidays();
+	}
+
+}
 
