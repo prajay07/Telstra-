259c46cc7e8b10faecdfab5d705e1dd84c6733ff
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:48:41 +0530

    Update UserDefinedCheckedException.java

--- day6/Exception/UserDefinedCheckedException.java
+++ day6/Exception/UserDefinedCheckedException.java
@@ -1,1 +1,47 @@
+class userInvalidException extends Exception
+{
+	String msg;
 
+	public  userInvalidException(String msg) {
+		super();
+		this.msg = msg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+
+class Customer2 
+	{
+		public void CheckEligibility(int age) throws userInvalidException {
+			if(age>=18)
+				System.out.println("Elgible to vote");
+			else
+				throw new userInvalidException("age is not valid");
+		}
+	}
+
+public class UserDefinedCheckedException 
+{
+
+	public  void main(String[] args) 
+	{
+		
+		Customer2 customer2= new Customer2();
+		try
+		{
+			customer2.CheckEligibility(4);
+		}
+		catch (userInvalidException ae) 
+		{
+			System.out.println(ae.getMsg());
+		}
+		
+	}
+}
+}
