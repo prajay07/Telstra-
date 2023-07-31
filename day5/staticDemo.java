ab8923c5a7b1954cf6e64da4b4ea7e46d92052e0
Prajay Joshi <106602678+prajay07@users.noreply.github.com>
2023-07-31 22:57:07 +0530

    Update staticDemo.java

--- day5/staticDemo.java
+++ day5/staticDemo.java
@@ -1,1 +1,17 @@
+package telstra.day5.staticDemo;
 
+public class StaticDemo {
+
+	static {
+		System.out.println("THis is a static block 1");
+	}
+	public static void main(String[]args)
+	{
+		System.out.println("THis is a main method");
+	}
+	
+	static 
+	{
+		System.out.println("THis is a static block 2");
+	}
+}
